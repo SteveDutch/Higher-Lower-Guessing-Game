61123ccc4389c6f990441cf07f179041531bbdb7
SteveDutch <steholl@posteo.org>
2022-02-20 21:03:39 +0100

    brighten up code

--- stefan-assignment-2/src/com/coderscampus/HigherLowerGuessingGame.java
+++ stefan-assignment-2/src/com/coderscampus/HigherLowerGuessingGame.java
@@ -6,22 +6,17 @@ import java.util.Scanner;
 public class HigherLowerGuessingGame {
 
 	public static void main(String[] args) {
-		
+
 		Random random = new Random();
 		int theRandomNumber = random.nextInt(101);
 		boolean lost = true;
-		
-		int i =1;
-		while (i <=5) {
-			int checkGuess =pickANumber();
-			if (checkGuess <1 || checkGuess >100) {
+
+		int i = 1;
+		while (i <= 5) {
+			int checkGuess = pickANumber();
+			if (checkGuess < 1 || checkGuess > 100) {
 				System.out.println("\nYour guess is not between 1 and 100, please try again");
-				// checkGuess = pickANumber();
-				
-			}
-		
-			
-			else if (checkGuess < theRandomNumber) {
+			} else if (checkGuess < theRandomNumber) {
 				System.out.println("\nPlease pick a higher number");
 				i++;
 			} else if (checkGuess > theRandomNumber) {
@@ -29,37 +24,24 @@ public class HigherLowerGuessingGame {
 				i++;
 			} else if (checkGuess == theRandomNumber) {
 				System.out.println("\nYou win!");
-				i =6;
-				lost = false; 
-			
+				i = 6;
+				lost = false;
 			}
 		}
-		
+
 		if (lost == true) {
 			System.out.println("\nYou lose!");
 			System.out.println("\nThe number to guess was: " + theRandomNumber);
-			
-			
 		}
-		
 
 	}
 
 	public static int pickANumber() {
 		System.out.print("\nPick a number between 1 and 100 ");
-		Scanner scanner = new Scanner (System.in);
+		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 		int pickedNumber = Integer.parseInt(input);
-		// Das gehört zur Schleife
-		/*
-		 * if (pickedNumber <1 || pickedNumber >100) {
-		 * System.out.println("Your guess is not between 1 and 100, please try again");
-		 * pickANumber();
-		 * 
-		 * }
-		 */
 		return pickedNumber;
-		
 	}
 
 }
